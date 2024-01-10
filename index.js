e98b4705cad03894b624fc48a69b8ab84df11df6
David-Zik <davidezeike6@gmail.com>
2024-01-10 05:27:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
+const allLinks = document.getElementsByTagName("a");
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle("nav--visibility");
+});
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    nav.classList.add("nav--visibility");
+  });
+});
+
+console.log(allLinks);
+
+allLinks.addEventListener("click", (event) => {
+  event.preventDefault();
+});
